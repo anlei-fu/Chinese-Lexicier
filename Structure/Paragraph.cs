bb536351b5030a67efe3238d53680fc7e5f36dc7
anlei-fu <767550758@qq.com>
2018-05-25 16:55:43 +0800

    Create Paragraph.cs

---
+++ Structure/Paragraph.cs
@@ -0,0 +1,10 @@
+using ConsoleApp1.Chinese._Sentence;
+using Fal.Chinese.InterFace;
+
+namespace ConsoleApp1.Chinese._Paragraph
+{
+    public    class Paragraph:BasicProperty<Sentence>
+    {
+      
+    }
+}
