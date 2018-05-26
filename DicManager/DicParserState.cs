f8cce84fc91e3efa26711f3d648d5f7132f2872b
anlei-fu <767550758@qq.com>
2018-05-26 15:03:36 +0800

    Create DicParserState.cs

---
+++ DicManager/DicParserState.cs
@@ -0,0 +1,13 @@
+namespace Test.Chinese.DicManager
+{
+    public   enum DicParserState
+    {
+        Default,
+        Name,
+        Type,
+        PreType,
+        Frequency,
+        Pinyin,
+
+    }
+}
